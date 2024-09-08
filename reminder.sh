9560d400fe4ecf0b032285e6a82100fa215293b5
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2024-09-08 10:57:59 -0400

    Update reminder.sh

--- reminder.sh
+++ reminder.sh
@@ -7,12 +7,10 @@ while true; do
     # Sleep for 20 minutes (1200 seconds)
     sleep 1200
 
-    # Get the idle time in milliseconds
+    # get idle time
     IDLE_TIME=$(xprintidle)
 
-    # Check if the idle time is less than the threshold
     if [ "$IDLE_TIME" -lt "$IDLE_THRESHOLD" ]; then
-        # If user is active, show the popup reminder
         zenity --info --text="Take a 20-second break! Look 20 feet away!" --title="20-20-20 Reminder"
     fi
 done
